ff12c9a8dce0512ab5737db0e5ed6f9c2767a5f0
hoangduc <duchh@apero.vn>
2026-09-04 09:57:44 +0700

    Initial Commit

---
+++ editimage/editimageApp.swift
@@ -0,0 +1,32 @@
+//
+//  editimageApp.swift
+//  editimage
+//
+//  Created by Hoàng Đức on 4/9/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct editimageApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
